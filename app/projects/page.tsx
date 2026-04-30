2fded18e4ed257e78e8ccfa548cd97052c4a3910
Taha Rawjani <tahakazim08@gmail.com>
2026-04-30 12:30:11 -0400

    proj

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -6,31 +6,31 @@ export default function ProjectsPage() {
   const projects = [
     {
       title: "Neo OS",
-      tech: "C++, x86 Assembly, Custom Drivers",
+      tech: "c++, x86 nasm, qemu",
       link: "https://github.com/2900xt/neo-OS",
       description: "An operating system written from scratch featuring custom drivers, UNIX-based VFS, multithreading, interactive shell with TahaScript."
     },
     {
       title: "MobyGlobal: AI Whale Tracking",
-      tech: "TensorFlow, Python, Next.js, Embedded C++",
+      tech: "tensorflow, python, c++, embeded systems, flask, platform-io",
       link: "https://mobylabs.org",
       description: "Real-time AI-powered whale tracking network using 3D-printed buoys."
     },
     {
       title: "67-Racer",
-      tech: "mosquitto, nextjs, opencv, python, pytorch, aws deepracers",
+      tech: "mosquitto, nextjs, opencv, python, aws deepracers",
       link: "https://ghost-racer.vercel.app/",
       description: " Clones how you drive. Then beats you at your own game."
     },
     {
       title: "Mobylabs: Automating AI Research",
-      tech: "Next.js, LangGraph, Supabase, Python",
+      tech: "next.js, langchain, python, arxiv API, anthropic, openai",
       link: "https://github.com/2900xt/mobylabs",
       description: "Fully automated AI research platform. Scrapes ArXiv for papers, generates code implementations, and drafts methodology and results sections for research papers."
     },
     {
       title: "Nexus: AI Learning Platform",
-      tech: "LangGraph, RAG, Kaplay.js, ElevenLabs, Next.js",
+      tech: "langgraph, kaplay.js, next.js, python, digitalocean, elevenlabs",
       link: "https://nexus-rust-theta.vercel.app/landing",
       description: "Agentically generates interactive mini-games and videos from teacher lesson plans in one click."
     }
